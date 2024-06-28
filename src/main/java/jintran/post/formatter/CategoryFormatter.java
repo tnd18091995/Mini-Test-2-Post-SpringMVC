03dcc98d00432ea16ad286a150a36e7a8db79688
tnd18091995 <tnd18091995@gmail.com>
2024-06-28 15:07:14 +0700

    create formatter

---
+++ src/main/java/jintran/post/formatter/CategoryFormatter.java
@@ -0,0 +1,29 @@
+//package jintran.post.formatter;
+//
+//import jintran.post.model.Category;
+//import jintran.post.service.ICategoryService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.format.Formatter;
+//import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Locale;
+//import java.util.Optional;
+//
+//
+//public class CategoryFormatter implements Formatter<Category> {
+//    private final ICategoryService categoryService;
+//    @Autowired
+//    public CategoryFormatter(ICategoryService categoryService) {
+//        this.categoryService = categoryService;
+//    }
+//    @Override
+//    public Category parse(String text, Locale locale){
+//        Optional<Category> categoryOptional = categoryService.findById(Long.parseLong(text));
+//        return categoryOptional.orElse(null);
+//    }
+//    @Override
+//    public String print(Category object, Locale locale){
+//        return "[" + object.getId() + "," + object.getName() + "]";
+//    }
+//}
