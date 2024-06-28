6e97dc94f84d7e457988da9763c4dd7d95e0ce49
tnd18091995 <tnd18091995@gmail.com>
2024-06-28 14:07:12 +0700

    create appConfig & AppInit

---
+++ src/main/java/jintran/post/configuration/AppInitializer.java
@@ -0,0 +1,20 @@
+package jintran.post.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{AppConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
