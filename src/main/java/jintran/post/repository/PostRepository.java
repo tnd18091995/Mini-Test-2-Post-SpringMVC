043ce29f907218665213388576225ed2d042347f
tnd18091995 <tnd18091995@gmail.com>
2024-06-28 14:27:25 +0700

    up repository

---
+++ src/main/java/jintran/post/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package jintran.post.repository;
+
+import jintran.post.model.post.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
